bed1acb52805e725f89516c1d0d8a5f0a5983d0d
kassaygyongyver <kassaygyongyver@gmail.com>
2017-05-09 21:59:51 +0200

    implementation of TicketDaoImpl.java

--- bugtracker-server/dao/src/main/java/hu/elte/inf/software/technology/bugtracker/dao/TicketDaoImpl.java
+++ bugtracker-server/dao/src/main/java/hu/elte/inf/software/technology/bugtracker/dao/TicketDaoImpl.java
@@ -2,6 +2,8 @@ package hu.elte.inf.software.technology.bugtracker.dao;
 
 import java.util.List;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.springframework.stereotype.Repository;
 
 import hu.elte.inf.software.technology.bugtracker.domain.Ticket;
@@ -9,34 +11,45 @@ import hu.elte.inf.software.technology.bugtracker.domain.Ticket;
 @Repository
 public class TicketDaoImpl implements TicketDao {
 
+	private SessionFactory sessionFactory;
+	
+	public void setSessionFactory(SessionFactory sf){
+		this.sessionFactory = sf;
+	}
 	@Override
 	public void addTicket(Ticket ticket) {
-		// TODO Auto-generated method stub
-		
+		Session session = this.sessionFactory.getCurrentSession();
+		session.persist(ticket);
 	}
 
 	@Override
 	public void updateTicket(Ticket ticket) {
-		// TODO Auto-generated method stub
-		
+		Session session = this.sessionFactory.getCurrentSession();
+		session.update(ticket);
 	}
 
 	@Override
+	@SuppressWarnings("unchecked")
 	public List<Ticket> listTickets() {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = this.sessionFactory.getCurrentSession();
+		List<Ticket> ticketsList = session.createQuery("from Tickets").list();
+		return ticketsList;
 	}
 
 	@Override
 	public Ticket getTicketById(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		Session session = this.sessionFactory.getCurrentSession();		
+		Ticket ticket = (Ticket) session.load(Ticket.class, new Integer(id));
+		return ticket;
 	}
 
 	@Override
 	public void removeTicket(int id) {
-		// TODO Auto-generated method stub
-		
+		Session session = this.sessionFactory.getCurrentSession();
+		Ticket ticket = (Ticket) session.load(Ticket.class, new Integer(id));
+		if(null != ticket){
+			session.delete(ticket);
+		}
 	}
 
 }
