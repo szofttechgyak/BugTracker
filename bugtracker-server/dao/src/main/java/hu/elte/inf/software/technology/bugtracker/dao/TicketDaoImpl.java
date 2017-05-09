5642a3c1b31ed7549a0771ea0c184b0fcd072016
kassaygyongyver <kassaygyongyver@gmail.com>
2017-05-09 21:35:06 +0200

    Ticket Dao changes

---
+++ bugtracker-server/dao/src/main/java/hu/elte/inf/software/technology/bugtracker/dao/TicketDaoImpl.java
@@ -0,0 +1,42 @@
+package hu.elte.inf.software.technology.bugtracker.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import hu.elte.inf.software.technology.bugtracker.domain.Ticket;
+
+@Repository
+public class TicketDaoImpl implements TicketDao {
+
+	@Override
+	public void addTicket(Ticket ticket) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateTicket(Ticket ticket) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Ticket> listTickets() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Ticket getTicketById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void removeTicket(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
