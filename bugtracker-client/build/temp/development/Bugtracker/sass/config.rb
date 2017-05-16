83a365385cc466cb78ee9dfb08820b7ec2157874
Mike Olasz <mikeolasz94@gmail.com>
2017-05-16 20:32:32 +0200

    and these?

--- bugtracker-client/build/temp/development/Bugtracker/sass/config.rb
+++ bugtracker-client/build/temp/development/Bugtracker/sass/config.rb
@@ -1,2 +1,2 @@
 Compass.add_project_configuration('..\..\..\..\..\sass\config.rb')
-cache_path = 'C:\Users\kalex1994\Documents\BugTracker\bugtracker-client/build/.sass-cache'
+cache_path = 'C:\Users\mike\Documents\BugTracker\bugtracker-client/build/.sass-cache'
