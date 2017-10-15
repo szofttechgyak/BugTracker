b4efd19c2302e9ac4ea742a0941d745e5c6c635b
Alex Kiraly <alex_kiraly@epam.com>
2017-10-15 22:12:38 +0200

    get assigned projects for user, and get assigned users for project query added

--- bugtracker-server/service/src/main/java/hu/elte/inf/software/technology/bugtracker/service/ProjectUserService.java
+++ bugtracker-server/service/src/main/java/hu/elte/inf/software/technology/bugtracker/service/ProjectUserService.java
@@ -1,11 +1,14 @@
 package hu.elte.inf.software.technology.bugtracker.service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import hu.elte.inf.software.technology.bugtracker.domain.Project;
 import hu.elte.inf.software.technology.bugtracker.domain.ProjectUser;
+import hu.elte.inf.software.technology.bugtracker.domain.User;
 import hu.elte.inf.software.technology.bugtracker.projectuserdao.ProjectUserDao;
 
 @Service
@@ -20,6 +23,22 @@ public class ProjectUserService {
     
     public ProjectUser getProjectUserById(int id) {
     	return projectUserDao.getProjectUserById(id);
+    }
+    
+    public List<Project> getAssignedProjects(int userId) {
+    	return getAllProjectUsers()
+    			.stream()
+    			.filter(projectUser -> projectUser.getUser().getId() == userId)
+    			.map(projectUser -> projectUser.getProject())
+    			.collect(Collectors.toList());
+    }
+    
+    public List<User> getAssignedUsers(int projectId) {
+    	return getAllProjectUsers()
+    			.stream()
+    			.filter(projectUser -> projectUser.getProject().getId() == projectId)
+    			.map(projectUser -> projectUser.getUser())
+    			.collect(Collectors.toList());
     }
     
     public void addProjectUser(ProjectUser projectUser){
